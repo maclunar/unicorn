0a9b4ed8447dc11ce900263a343fd95442878bd1
maclunar <maclunar@gmail.com>
2017-07-09 14:38:36 +0200

    Generate and migrate User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709123659) do
+ActiveRecord::Schema.define(version: 20170709123752) do
 
   create_table "blug_articles", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20170709123659) do
   create_table "blug_comments", force: :cascade do |t|
     t.integer  "article_id"
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
